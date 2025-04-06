a35f79c24c940e6d34736b36f1991e8dd3256e23
NGUYEN NGA <MIDEA>
2025-04-06 21:06:14 +0700

    Trang chu

---
+++ TrangChu/TrangChu.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next", // Selector cho span next
+    prevEl: ".swiper-button-prev", // Selector cho span prev
+  },
+});
