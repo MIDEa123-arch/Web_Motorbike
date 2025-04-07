7bec8ded639b209a551cfee2468bb718195eae2e
NGUYEN NGA <MIDEA>
2025-04-07 14:59:08 +0700

    Mot nua phan tin tuc

--- TrangChu/TrangChu.js
+++ TrangChu/TrangChu.js
@@ -7,3 +7,10 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev", // Selector cho span prev
   },
 });
+
+// Lắng nghe sự kiện click vào thẻ card
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", function () {
+    this.classList.toggle("active"); // Thêm hoặc bỏ lớp 'active' khi click
+  });
+});
