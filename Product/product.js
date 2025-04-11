6a8279d4545d2472e2b145469388183644b436db
NGUYEN NGA <MIDEA>
2025-04-11 22:34:35 +0700

    Xong link xe

---
+++ Product/product.js
@@ -0,0 +1,38 @@
+const filterButtons = document.querySelectorAll(".menu button");
+const filterableSections = document.querySelectorAll(".product .pro");
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    document.querySelector(".menu .active")?.classList.remove("active");
+    e.target.classList.add("active");
+
+    const selectedType = e.target.getAttribute("data-type");
+    filterSections(selectedType);
+  });
+});
+
+function filterSections(type) {
+  filterableSections.forEach((section) => {
+    const sectionType = section.getAttribute("data-type");
+    const title = section.querySelector(".type");
+
+    if (type === "All") {
+      section.style.display = "block";
+      if (title) title.style.display = "block"; // Hiện tiêu đề
+    } else if (sectionType === type) {
+      section.style.display = "block";
+      if (title) title.style.display = "none"; // Ẩn tiêu đề
+    } else {
+      section.style.display = "none";
+    }
+  });
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const filterType = urlParams.get("filter");
+
+if (filterType) {
+  filterSections(filterType);
+} else {
+  filterSections("All");
+}
